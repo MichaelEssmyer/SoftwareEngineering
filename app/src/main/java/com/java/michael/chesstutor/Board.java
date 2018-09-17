c4ea38aad9f178b5432601df4f7e9130a0dfd0eb
Michael Essmyer <mde9rb@mail.umkc.edu>
2018-09-16 21:21:29 -0500

    .gitignore fix

---
+++ app/src/main/java/com/java/michael/chesstutor/Board.java
@@ -0,0 +1,7 @@
+package com.java.michael.chesstutor;
+
+import android.widget.ImageButton;
+
+public class Board {
+
+}
---
+++ app/src/test/java/com/java/michael/chesstutor/MainActivityTest.java
@@ -0,0 +1,91 @@
+package com.java.michael.chesstutor;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    ImageButton h1,h2,h3,h4,h5,h6,h7,h8;
+    ImageButton g1,g2,g3,g4,g5,g6,g7,g8;
+    ImageButton f1,f2,f3,f4,f5,f6,f7,f8;
+    ImageButton e1,e2,e3,e4,e5,e6,e7,e8;
+    ImageButton d1,d2,d3,d4,d5,d6,d7,d8;
+    ImageButton c1,c2,c3,c4,c5,c6,c7,c8;
+    ImageButton b1,b2,b3,b4,b5,b6,b7,b8;
+    ImageButton a1,a2,a3,a4,a5,a6,a7,a8;
+
+    @Test
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        a1 = findViewById(R.id.a1Button);
+        a2 = findViewById(R.id.a2Button);
+        a3 = findViewById(R.id.a3Button);
+        a4 = findViewById(R.id.a4Button);
+        a5 = findViewById(R.id.a5Button);
+        a6 = findViewById(R.id.a6Button);
+        a7 = findViewById(R.id.a7Button);
+        a8 = findViewById(R.id.a8Button);
+        b1 = findViewById(R.id.b1Button);
+        b2 = findViewById(R.id.b2Button);
+        b3 = findViewById(R.id.b3Button);
+        b4 = findViewById(R.id.b4Button);
+        b5 = findViewById(R.id.b5Button);
+        b6 = findViewById(R.id.b6Button);
+        b7 = findViewById(R.id.b7Button);
+        b8 = findViewById(R.id.b8Button);
+        c1 = findViewById(R.id.c1Button);
+        c2 = findViewById(R.id.c2Button);
+        c3 = findViewById(R.id.c3Button);
+        c4 = findViewById(R.id.c4Button);
+        c5 = findViewById(R.id.c5Button);
+        c6 = findViewById(R.id.c6Button);
+        c7 = findViewById(R.id.c7Button);
+        c8 = findViewById(R.id.c8Button);
+        d1 = findViewById(R.id.d1Button);
+        d2 = findViewById(R.id.d2Button);
+        d3 = findViewById(R.id.d3Button);
+        d4 = findViewById(R.id.d4Button);
+        d5 = findViewById(R.id.d5Button);
+        d6 = findViewById(R.id.d6Button);
+        d7 = findViewById(R.id.d7Button);
+        d8 = findViewById(R.id.d8Button);
+        e1 = findViewById(R.id.e1Button);
+        e2 = findViewById(R.id.e2Button);
+        e3 = findViewById(R.id.e3Button);
+        e4 = findViewById(R.id.e4Button);
+        e5 = findViewById(R.id.e5Button);
+        e6 = findViewById(R.id.e6Button);
+        e7 = findViewById(R.id.e7Button);
+        e8 = findViewById(R.id.e8Button);
+        f1 = findViewById(R.id.f1Button);
+        f2 = findViewById(R.id.f2Button);
+        f3 = findViewById(R.id.f3Button);
+        f4 = findViewById(R.id.f4Button);
+        f5 = findViewById(R.id.f5Button);
+        f6 = findViewById(R.id.f6Button);
+        f7 = findViewById(R.id.f7Button);
+        f8 = findViewById(R.id.f8Button);
+        g1 = findViewById(R.id.g1Button);
+        g2 = findViewById(R.id.g2Button);
+        g3 = findViewById(R.id.g3Button);
+        g4 = findViewById(R.id.g4Button);
+        g5 = findViewById(R.id.g5Button);
+        g6 = findViewById(R.id.g6Button);
+        g7 = findViewById(R.id.g7Button);
+        g8 = findViewById(R.id.g8Button);
+        h1 = findViewById(R.id.h1Button);
+        h2 = findViewById(R.id.h2Button);
+        h3 = findViewById(R.id.h3Button);
+        h4 = findViewById(R.id.h4Button);
+        h5 = findViewById(R.id.h5Button);
+        h6 = findViewById(R.id.h6Button);
+        h7 = findViewById(R.id.h7Button);
+        h8 = findViewById(R.id.h8Button);
+        //createButtons();
+    }
+}
